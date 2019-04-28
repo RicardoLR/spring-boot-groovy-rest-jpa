17f075968c6b75a90318253d86c0bbbc087164d1
Ricardo <rlopez@interware.com.mx>
2019-04-28 12:41:46 -0500

    proyecto inicializado

---
+++ src/main/groovy/com/example/demogroovyapirest/DemoGroovyApiRestApplication.groovy
@@ -0,0 +1,13 @@
+package com.example.demogroovyapirest
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class DemoGroovyApiRestApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(DemoGroovyApiRestApplication, args)
+	}
+
+}
